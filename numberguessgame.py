090d3cf93d330831e99a2fc3f0dad83502806fc2
Aniketgoyal12 <143060619+Aniketgoyal12@users.noreply.github.com>
2024-03-11 10:14:07 +0530

    Update numberguessgame.py

--- numberguessgame.py
+++ numberguessgame.py
@@ -1,1 +1,23 @@
-
+import random
+com = random.randint(1,101)
+score = 5
+#print(com)
+while 1:
+    user = int(input("Enter the Number "))
+    if user == com :
+        print("You Won with", score, "attempts.")
+        break
+    elif user > com:
+        print("Too Large")
+        score -= 1
+        print("you left", score, "attempts")
+        if score == 0:
+            print("you lose")
+            break
+    else:
+        print("Too Small")
+        score -= 1
+        print("you left", score, "attempts")
+        if score == 0:
+            print("you lose")
+            break
